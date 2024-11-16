6fdca7e083f869e6222e7758fdb847a9e10c4206
nur <nurchowdhury56@gmail.com>
2024-11-16 23:39:12 +0600

    vite resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// export default {
+//   build: {
+//     outDir: "dist", // default
+//   },
+// };
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5174',
-        secure: false,
-      }
-    }
-  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
+});
